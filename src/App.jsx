b7f2875c9cad36e58e33f9345c03d89ea01cd342
Jhoan2-cpu <antoni.sagitario21@gmail.com>
2023-04-10 06:53:41 -0500

    se agregaron nuevos ficheros y ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ function App() {
   return (
     <div>
       <h1>No ocurrio nada</h1>
-      <Target />
+      <Target direct={"Jhoan/NoNeFYoyu"} description={"esto es una descripción"} button={["🌟 Star","End", "Jsx","HTML", "CSS3"]} moreInf={[334, "PLSQL", "GPL-3.0 license", "Updated on Dec 12, 2022"]}/>
+      <Target direct={"Carlos/Ultimate"} description={"última actualización"} button={["🌟 Star","End", "Js","HTML"]} moreInf={[6334, "Jupyter Notebook", "MIT license", "Updated on Feb 8, 2023"]}/>
     </div>
   )
 }
--- src/target.jsx
+++ src/target.jsx
@@ -1,13 +1,31 @@
+import { FaBeer } from "react-icons/fa";
+import {BsStar, BsJournalBookmark} from "react-icons/bs";
+import { SiAbbrobotstudio } from "react-icons/si";
+
 import "./target.css"
 function Target({direct, description, button, moreInf}){
     return (
         <div className="target">
-            <div><h1>📓</h1></div>
-            <div>
+            <div className="target_child"><h1 className="icon"><BsJournalBookmark /></h1></div>
+            <div className="target_child">
                 <div><a href="#">{direct}</a></div>
-                <div><p>{description}</p></div>
-                <div></div>
-                <div></div>
+                <div className="mid"><p>{description}</p></div>
+                <div>
+                    {button.map((user, ind)=>{
+                        return <button key={ind}>
+                            {user}
+                        </button>
+                    })}
+                </div>
+                <div className="ult">
+                    {
+
+                    }
+                    <span className="span_in"><BsStar /> {moreInf[0]}</span>
+                    <span className="span_in"><span className="span_circle"></span> {moreInf[1]}</span>
+                    <span className="span_in">{moreInf[2]}</span>
+                    <span className="span_in">{moreInf[3]}</span>
+                    </div>
             </div>
         </div>
     )
